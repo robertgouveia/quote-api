4022c54259f9157f688f222159c61251343e56cd
robertgouveia <robertgouveia03@hotmail.com>
2024-03-07 18:14:54 +0000

    Removed front end.

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const { getRandomElement } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
-
 app.listen(PORT)
 
 const quoteRouter = express.Router()
